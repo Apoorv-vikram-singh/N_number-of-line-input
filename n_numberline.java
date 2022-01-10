b0bef5d65fea72e82594f01e265dc0d6b1190d79
Apoorv-vikram-singh <90571448+Apoorv-vikram-singh@users.noreply.github.com>
2022-01-10 15:37:27 +0530

    Add files via upload

---
+++ n_numberline.java
@@ -0,0 +1,15 @@
+    import java.util.*;
+    public class n_numberline {
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner (System.in);
+		int i=1;
+		while(sc.hasNext())
+		{
+		    String name=sc.nextLine();
+		    System.out.println(i+" "+name);
+		    i++;
+		}
+	
+	}
+}
